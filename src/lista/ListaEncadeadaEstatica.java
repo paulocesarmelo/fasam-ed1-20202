c66871257c646e0a121b817843f85c3e7ba612f0
Paulo César Melo <paulocesarmelo@MacBook-Pro-de-Paulo.local>
2020-09-23 22:44:33 -0300

    update

--- src/lista/ListaEncadeadaEstatica.java
+++ src/lista/ListaEncadeadaEstatica.java
@@ -4,9 +4,9 @@ package lista;
 public class ListaEncadeadaEstatica {
 	
 	//atributos
-	int[] elementos;
-	int qtdElementos = 0;
-	int tamanho;
+	private int[] elementos;
+	private int qtdElementos = 0;
+	private int tamanho;
 	
 	public ListaEncadeadaEstatica(int tam) {
 		tamanho = tam;
@@ -14,7 +14,7 @@ public class ListaEncadeadaEstatica {
 	}
 	
 	//métodos
-	void adicionaNoInicio(int elemento) {
+	public void adicionaNoInicio(int elemento) {
 		
 		if(qtdElementos == tamanho) {
 			System.out.println("O vetor está cheio!");
@@ -34,16 +34,122 @@ public class ListaEncadeadaEstatica {
 		
 	}
 	
-	void adicionaNoFinal(int elemento) {}
+	public void adicionaNoFinal(int elemento) {
+		
+		if(tamanho == qtdElementos) {
+			System.out.println("A lista está cheia!");
+		}else {
+			
+			elementos[qtdElementos] = elemento;
+			qtdElementos++;
+			
+		}
+		
+	}
 	
-	void adicionaNaPosicao(int elemento, int posicao) {}
+	public void adicionaNaPosicao(int elemento, int posicao) {
+		
+		if( posicao == 0 ) {
+			adicionaNoInicio(elemento);
+		}else if (posicao == qtdElementos) {
+			adicionaNoFinal(elemento);
+		}else {
+			
+			for( int i = qtdElementos; i > posicao; i-- ) {
+				elementos[i] = elementos[i-1];
+			}
+			
+			elementos[posicao] = elemento;
+			qtdElementos++;
+			
+		}
+		
+	}
 	
-	void imprimirLista(){
+	public void removerDoInicio() {
+		
+		if( qtdElementos == 0) {
+			System.out.println("A lista está vazia!");
+		}else {
+			
+			if( qtdElementos == 1) {
+				qtdElementos--;
+			}else {
+				
+				for(int i=0; i < qtdElementos-1; i++) {
+					elementos[i] = elementos[i+1];
+				}
+				
+				qtdElementos--;
+				
+			}
+			
+		}
+		
+	}
+
+	public void removerDoFinal() {
+		
+		if( qtdElementos == 0 ) {
+			System.out.println("Lista está vazia!");
+		}else {
+			//elementos[qtdElementos-1] = 0; sendo que 0 seria um valor não permitido na lista
+			qtdElementos--;
+		}
+	}
+	
+	public void removerDaPosicao(int pos) {
+		
+		if(qtdElementos == 0) {
+			System.out.println("A lista está vazia!");
+		}else {
+			
+			if( pos == 0) {
+				removerDoInicio();
+			}else if( pos == qtdElementos-1) {
+				removerDoFinal();
+			}else {
+				
+				for( int i = pos; i < qtdElementos-1; i++) {
+					elementos[i] = elementos[i+1];
+				}
+				
+				qtdElementos--;
+				
+			}
+			
+		}
+		
+	}
+	
+	public int busca(int elemento) {
+		
+		if(qtdElementos == 0) {
+			System.out.println("Lista vazia!");
+		}else {
+			
+			for(int i=0; i < qtdElementos; i++) {
+				if(elementos[i] == elemento) {
+					System.out.println("Elemento está na lista!");
+					return i;
+				}
+			}
+			
+			System.out.println("Elemento não está na lista!");
+			
+		}
+		return -1; //quando não encontrar ou quando a lista estiver vazia
+		
+	}
+	
+	public void imprimirLista(){
 		
 		System.out.print("Lista: ");
 		for(int i=0; i < qtdElementos; i++) {
 			System.out.print(elementos[i]+"\t");
 		}
+		
+		System.out.println("\n");
 		
 	}
 	
--- src/lista/PrincipalLista.java
+++ src/lista/PrincipalLista.java
@@ -1,5 +1,7 @@
 package lista;
 
+import java.util.Scanner;
+
 public class PrincipalLista {
 
 	public static void main(String[] args) {
@@ -7,10 +9,47 @@ public class PrincipalLista {
 		ListaEncadeadaEstatica lista = new ListaEncadeadaEstatica(10);
 		
 		lista.adicionaNoInicio(3);
+		
+		lista.imprimirLista();
+		
 		lista.adicionaNoInicio(5);
+		
+		lista.imprimirLista();
+		
 		lista.adicionaNoInicio(10);
 		
 		lista.imprimirLista();
+		
+		lista.adicionaNoFinal(4);
+		
+		lista.imprimirLista();
+		
+		lista.adicionaNaPosicao(7, 1);
+		
+		lista.imprimirLista();
+		
+		lista.removerDoInicio();
+		
+		lista.imprimirLista();
+		
+		lista.removerDoFinal();
+		
+		lista.imprimirLista();
+		
+		lista.removerDaPosicao(1);
+		
+		//System.out.println("\n\nLista após remoção\n\n");
+		
+		lista.imprimirLista();
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Informe um elemento para busca: ");
+		int elemento = input.nextInt();
+		
+		lista.busca(elemento);
+		
 		
 	}
 	
